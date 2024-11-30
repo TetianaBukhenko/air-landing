aa447b992c2b2043b91656d137ea9918bd6b77a7
unknown <tania04bukhenko@gmail.com>
2024-11-30 15:26:30 +0200

    add landing code

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Air landing page
+This landing page is designed for a strategic agency with a focus on an engaging user experience. It includes dynamic theme switching, an interactive slider, and form validation—all powered by JavaScript. The responsive layout ensures smooth functionality on both mobile and desktop devices.
+
+## Demo
+https://tetianabukhenko.github.io/museum-landing/
+
+## Design
+https://www.figma.com/file/7qwsWggv9BAxMi2VPhBuPr/Air-(formerly-Dia)?node-id=9138%3A35
+
+## Technologies
+-HTML5, SCSS, JavaScript
+
+## Features
+1. *Loading on Scroll*: The Intersection Observer API is used to delay content loading until it is about to appear on the screen, improving performance.
+2. *nteractive Layout & Smooth Scrolling*: Engaging animations and transitions were implemented to enhance the user experience.
+3. *Theme Switching*: A built-in feature allows users to toggle between light and dark themes using JavaScript.
+4. *Slider*: An interactive slider displays key content dynamically, improving navigation and engagement.
+5. *Responsive Design*: The layout is fully responsive and adapts seamlessly across different devices, using Flexbox and Grid.
+6. *Form Validation*: A simple contact form validates user input to ensure proper data submission (without backend processing).
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/TetianaBukhenko/air-landing.git
+```
+
+Go to the project directory
+
+```bash
+  cd air-landing
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,203 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict';
+
+// #region switch-theme
+const button = document.getElementById('theme-switcher');
+const mainImg = document.getElementById('main-img');
+
+function changeTheme() {
+  if (document.body.classList.contains('pink-theme')) {
+    document.body.classList.remove('pink-theme');
+    button.firstElementChild.style.transform = 'translateX(0%)';
+  } else {
+    document.body.classList.add('pink-theme');
+    button.firstElementChild.style.transform = 'translateX(100%)';
+  }
+}
+
+button.addEventListener('click', changeTheme);
+// #endregion switch-theme
+
+// #region slider-handler
+const slides = document.querySelectorAll('.slide-item');
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener('DOMContentLoaded', initializeSlider);
+
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add('slide--fade-in', 'slide--show');
+    intervalId = window.setInterval(nextSlide, 2500);
+  }
+}
+
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = index;
+  }
+
+  slides.forEach((slide, i) => {
+    if (i === slideIndex) {
+      slide.classList.add('slide--fade-in', 'slide--show');
+      slide.classList.remove('slide--fade-out');
+    } else {
+      slide.classList.remove('slide--fade-in', 'slide--show');
+      slide.classList.add('slide--fade-out');
+    }
+  });
+}
+
+function prevSlide() {
+  clearInterval(intervalId);
+  showSlide(slideIndex - 1);
+  intervalId = window.setInterval(nextSlide, 2500);
+}
+
+function nextSlide() {
+  clearInterval(intervalId);
+  showSlide(slideIndex + 1);
+  intervalId = window.setInterval(nextSlide, 2500);
+}
+// #endregion
+
+// #region form-handler
+function clearErrors() {
+  document
+    .querySelectorAll('.form__input--hasError')
+    .forEach(function(element) {
+      element.classList.remove('form__input--hasError');
+    });
+
+  document
+    .querySelectorAll('.form__error-message')
+    .forEach(function(errorSpan) {
+      errorSpan.textContent = '';
+    });
+
+  document.getElementById('form__button').removeAttribute('disabled');
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  clearErrors();
+
+  const form = event.target;
+  const email = form.email.value;
+  const name = form.name.value;
+  const message = form.message.value;
+  const phone = form.phone.value;
+
+  let valid = true;
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!emailPattern.test(email)) {
+    valid = false;
+
+    document.getElementById('email').classList.add('form__input--hasError');
+
+    document.getElementById('email-error').textContent
+      = 'Please enter a valid email address (e.g. example@domain.com).';
+  }
+
+  if (name.trim() === '') {
+    valid = false;
+    document.getElementById('name').classList.add('form__input--hasError');
+
+    document.getElementById('name-error').textContent
+      = 'Please enter your name.';
+  }
+
+  if (message.trim() === '') {
+    valid = false;
+    document.getElementById('message').classList.add('form__input--hasError');
+
+    document.getElementById('message-error').textContent
+      = 'Please enter a message.';
+  }
+
+  const phonePattern = /^\+?\d{10,15}$/;
+
+  if (!phonePattern.test(phone)) {
+    valid = false;
+    document.getElementById('phone').classList.add('form__input--hasError');
+
+    document.getElementById('phone-error').textContent
+      = 'Please enter a valid phone number (e.g. +1234567890).';
+  }
+
+  if (valid) {
+    window.location.reload();
+  } else {
+    document.getElementById('form__button').disabled = true;
+  }
+}
+
+window.addEventListener('load', function() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+});
+// #endregion
+
+// #region menu
+const menuToggle = document.getElementById('icon--menu');
+const mobileMenu = document.getElementById('menu');
+let lastScrollPosition = 0;
+
+menuToggle.addEventListener('click', function() {
+  if (mobileMenu.classList.contains('aside__menu--hidden')) {
+    lastScrollPosition = window.scrollY;
+    mobileMenu.classList.remove('aside__menu--hidden');
+    document.body.style.overflow = 'hidden';
+    menuToggle.classList.remove('icon--menu');
+    menuToggle.classList.add('icon--close');
+  } else {
+    mobileMenu.classList.add('aside__menu--hidden');
+    document.body.style.overflow = '';
+    menuToggle.classList.remove('icon--close');
+    menuToggle.classList.add('icon--menu');
+    window.scrollTo(0, lastScrollPosition);
+  }
+});
+
+function onMenuLinkClick() {
+  mobileMenu.classList.add('aside__menu--hidden');
+  menuToggle.classList.remove('icon--close');
+  menuToggle.classList.add('icon--menu');
+  document.body.style.overflow = '';
+}
+// #endregion
+
+// #region intersectionObserver
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+
+      setTimeout(() => {
+        entry.target.classList.remove('hide', 'hide-left', 'hide-right');
+      }, 3000);
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hide');
+const hiddenElementsLeft = document.querySelectorAll('.hide-left');
+const hiddenElementsRight = document.querySelectorAll('.hide-right');
+const allHidden = [...hiddenElements].concat(
+  [...hiddenElementsLeft],
+  [...hiddenElementsRight],
+);
+
+allHidden.forEach((el) => observer.observe(el));
+// #endregion
